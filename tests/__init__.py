5a1ea073cf757a8bf8bc3deca78a9d101be24954
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:21:12 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part sale_cancel_pending_moves module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_sale_cancel_pending_moves import suite
-
-__all__ = ['suite']
